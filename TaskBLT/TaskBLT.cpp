88d29bb0a5abcf629134a3093844484ee0c87905
RazerFord <desk10567@gmail.com>
2022-02-16 12:19:14 +0700

    first commit

--- TaskBLT/TaskBLT.cpp
+++ TaskBLT/TaskBLT.cpp
@@ -1,9 +1,9 @@
-﻿// TaskBLT.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
 
-#include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //
+    return 0;
 }
